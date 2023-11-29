229a0a86bbdbb83c56708282cb3697a06210c88c
WinWinMaw <mawinwinmaw4@gmail.com>
2023-11-29 14:18:22 +0630

    conditional rendering

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -8,21 +8,31 @@ interface MyComponentProps {
 
 const MyComponent: React.FC<MyComponentProps> = ({ message }) => {
     const [count, setCount] = useState(0);
+    const [showMessage, setShowMessage] = useState(false);
 
-    
-  const handleButtonClick = () => {
-    setCount(count + 1);
-  };
+    const handleButtonClick = () => {
+        setCount(count + 1);
+    };
+
+    const handleToggleMessage = () => {
+        setShowMessage(!showMessage);
+      };
 
   return (
     <div className="bg-gray-200 p-4 rounded-md shadow-md">
     <p className="text-xl font-bold mb-2">{message}</p>
-    <p className="text-lg mb-4">Count: {count}</p>
+    {showMessage && <p className="text-lg mb-4">Count is visible: {count}</p>}
+    <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleButtonClick}
+      >
+        Increment Count
+      </button>
     <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={handleButtonClick}
+      onClick={handleToggleMessage}
     >
-      Increment Count
+      Toggle Message
     </button>
   </div>
   )
