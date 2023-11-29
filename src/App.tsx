3ce0c39ccbbe9c69121675e1506ebc0b30e8a545
WinWinMaw <mawinwinmaw4@gmail.com>
2023-11-29 15:11:38 +0630

    Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import React, { useState, useEffect } from 'react';
 import MyComponent from './components/MyComponent';
 
 const App: React.FC = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+  const apiUrl = 'http://localhost:5000/posts';
 
   return (
     <div>
       <h1>Hello from App!</h1>
-      <MyComponent messages={messages} setMessages={setMessages} />
+      <MyComponent apiUrl={apiUrl} />
     </div>                                    
   );
 };
--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,41 +1,41 @@
-import React, { useState } from 'react';
+// MyComponent.tsx
+import React, { useState, useEffect } from 'react';
 
 interface MyComponentProps {
-  messages: string[];
-  setMessages: React.Dispatch<React.SetStateAction<string[]>>;
+  apiUrl: string;
 }
 
-const MyComponent: React.FC<MyComponentProps> = ({ messages, setMessages }) => {
-  const [newMessage, setNewMessage] = useState('');
+interface Post {
+  id: number;
+  title: string;
+}
 
-  const handleAddMessage = () => {
-    setMessages([...messages, newMessage]);
-    setNewMessage('');
-  };
+const MyComponent: React.FC<MyComponentProps> = ({ apiUrl }) => {
+  const [data, setData] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [apiUrl]);
 
   return (
     <div className="bg-gray-200 p-4 rounded-md shadow-md">
       <ul>
-        {messages.map((message, index) => (
-          <li key={index} className="text-lg mb-2">
-            {message}
+        {data.map((post) => (
+          <li key={post.id} className="text-lg mb-2">
+            {post.title}
           </li>
         ))}
       </ul>
-      <div className="flex items-center mt-4">
-        <input
-          type="text"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          className="p-2 border border-gray-300 rounded-md mr-2"
-        />
-        <button
-          onClick={handleAddMessage}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Add Message
-        </button>
-      </div>
     </div>
   );
 };
